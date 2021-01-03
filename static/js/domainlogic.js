512387a1d92148b344a16d88faae21c9322011ae
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2021-01-02 21:11:56 -0500

    added lollipop chart of top ten words

--- static/js/domainlogic.js
+++ static/js/domainlogic.js
@@ -57,10 +57,77 @@ function generateWordCloud(wordArray) {
     }
 }
 
+// This function creates a lollipop chart of the top 10 most frequent words
+function generateLollipopChart(data){
+    // Set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 40, left: 100},
+        width = 460 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#lollipop-chart")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // sort data and pull top ten
+    data.sort(function(b, a) {
+    return a.frequency - b.frequency;
+    })
+
+    data = data.slice(0,10);
+
+    // Add X axis
+    var x = d3.scaleLinear()
+    .domain([0, data[0].frequency])
+    .range([ 0, width]);
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Y axis
+    var y = d3.scaleBand()
+    .range([ 0, height ])
+    .domain(data.map(function(d) { return d.keyword; }))
+    .padding(1);
+    svg.append("g")
+    .call(d3.axisLeft(y))
+
+    // Lines
+    svg.selectAll("myline")
+    .data(data)
+    .enter()
+    .append("line")
+        .attr("x1", function(d) { return x(d.frequency); })
+        .attr("x2", x(0))
+        .attr("y1", function(d) { return y(d.keyword); })
+        .attr("y2", function(d) { return y(d.keyword); })
+        .attr("stroke", "grey")
+
+    // Circles
+    svg.selectAll("mycircle")
+    .data(data)
+    .enter()
+    .append("circle")
+        .attr("cx", function(d) { return x(d.frequency); })
+        .attr("cy", function(d) { return y(d.keyword); })
+        .attr("r", "7")
+        .style("fill", "#69b3a2")
+        .attr("stroke", "black")
+}
+
+
 // Import the keywords data, generate the lollipop chart and word cloud and update as the user selects filters
 d3.json("/api/keywords").then((keywords) => {
     console.log(keywords)
     generateWordCloud(keywords)
+    generateLollipopChart(keywords)
 
 
 
